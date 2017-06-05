c45a7de9bb491536cdce6891a045a3d58dc266ae
Francesca di Centa <dicenta.francesca@gmail.com>
2017-06-05 13:07:19 +0200

    asking and answer from coach

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,5 +1,16 @@
 class CoachingController < ApplicationController
   def answer
+    @question = params[:query]
+    # here you just need the script
+    # @answers = RESTAURANTS.select {|r| r[:category] == @category }
+    if @question[-1] == "?"
+      @answer = "Silly question son!"
+    elsif @question == "I am going to work"
+      @answer = "Great, have a good day!"
+    else @question[-1] != "?"
+      @answer = "I don't care, you should go to work"
+    end
+    #because everything is passing through the answer !
   end
 
   def ask
